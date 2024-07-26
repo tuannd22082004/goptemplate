592b30ed744ad0a7a36031a6a03fa1f5ce4110e5
Nguyễn Đình Tuân <144453874+tuanndph33203@users.noreply.github.com>
2024-07-26 16:29:24 +0700

    feat: chuyển đổi các tệp CSS và SCSS sang Bootstrap và 1 phần SCSS #WL-8
    - Thêm các tệp SCSS mới: global.scss, home_page.scss, variable.scss
    - Thêm các tệp CSS mới: global.css, home_page.css, variable.css
    - Xóa tệp CSS thừa: style.css
    - Xóa tệp JS thừa: app.js
    - Cập nhật package.json và webpack.mix.js để tự động cấu hình
    - Sửa đổi index.html với Bootstrap và SCSS
    - Xóa mix-manifest.json thừa

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
-let mix = require('laravel-mix');
+let mix = require("laravel-mix");
 
-mix.js('public/js/app.js', 'dist/js').setPublicPath('public');
-mix.sass('public/scss/style.scss', 'public/dist/css');
+mix.sass("public/scss/global.scss", "public/dist/css");
+mix.sass("public/scss/variable.scss", "public/dist/css");
+
+mix.sass("public/scss/home_page.scss", "public/dist/css");
