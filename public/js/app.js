006781313dc2888e5f043d82c82ecb86c60e736d
Nguyễn Đình Tuân <144453874+tuanndph33203@users.noreply.github.com>
2024-07-29 15:11:56 +0700

    feat: chuyển đổi SCSS  sang Bootstrap và 1 phần SCSS tại trang index-3 #WL-8
    - Thêm các tệp hình ảnh mới: animation-image-2.png, animation-image.png, features-animation.png, home-animation-2.png
    - Thêm tệp CSS mới: home_page_3.css
    - Thêm tệp SCSS mới: home_page_3.scss
    - Cập nhật các tệp: mix-manifest.json, global.css, variable.css, index-3.html, app.js, global.scss, variable.scss, webpack.mix.js
    #WL-8

--- public/js/app.js
+++ public/js/app.js
@@ -6,9 +6,13 @@ function windowScroll() {
     document.body.scrollTop >= 50 ||
     document.documentElement.scrollTop >= 50
   ) {
-    navbar.classList.add("nav-sticky");
+    navbar.classList.add("bg-white");
+    navbar.classList.add("py-3");
+    navbar.classList.remove("py-4");
   } else {
-    navbar.classList.remove("nav-sticky");
+    navbar.classList.remove("bg-white");
+    navbar.classList.remove("py-3");
+    navbar.classList.add("py-4");
   }
 }
 
@@ -17,13 +21,10 @@ window.addEventListener("scroll", (ev) => {
   windowScroll();
 });
 
-
-
 // tiny slider
 
-
 var slider = tns({
-  container: '.client-slider',
+  container: ".client-slider",
   loop: true,
   autoplay: true,
   mouseDrag: true,
@@ -34,29 +35,26 @@ var slider = tns({
   speed: 900,
   animateIn: "fadeIn",
   animateOut: "fadeOut",
-  controlsText: ['&#8592;', '&#8594;'],
+  controlsText: ["&#8592;", "&#8594;"],
   autoplayButtonOutput: false,
   responsive: {
-    992:{
+    992: {
       gutter: 20,
-      items: 6
+      items: 6,
     },
     768: {
       gutter: 20,
-      items: 3
+      items: 3,
     },
-    576 :{
+    576: {
       gutter: 20,
-      items: 2
-    }
-  }
+      items: 2,
+    },
+  },
 });
 
-
-
-
 // ===========================================================
-// See tutorial at : 
+// See tutorial at :
 // mouse hover effect
 // ===========================================================
 
@@ -82,7 +80,7 @@ var slider = tns({
     },
     show: function () {
       return "(" + this.x + ", " + this.y + ")";
-    }
+    },
   };
 
   // Track the mouse position relative to the center of the container.
@@ -138,18 +136,15 @@ var slider = tns({
   container.onmouseenter = onMouseEnterHandler;
 })();
 
-
-
-
 // swiper slider
 
-new Swiper('.swiper-container', {
+new Swiper(".swiper-container", {
   loop: true,
   slidesPerView: 3,
   paginationClickable: true,
   spaceBetween: 20,
-  pagination: '.swiper-pagination',
-  slidesPerView: 'auto',
+  pagination: ".swiper-pagination",
+  slidesPerView: "auto",
   paginationClickable: true,
   spaceBetween: 0,
   centeredSlides: true,
@@ -157,24 +152,21 @@ new Swiper('.swiper-container', {
   breakpoints: {
     1920: {
       slidesPerView: 3,
-      spaceBetween: 50
+      spaceBetween: 50,
     },
     1028: {
       slidesPerView: 2,
-      spaceBetween: 30
+      spaceBetween: 30,
     },
     768: {
       slidesPerView: 1,
-      spaceBetween: 10
-    }
-  }
+      spaceBetween: 10,
+    },
+  },
 });
 
-
-
-
 // ===========================================================
-// See tutorial at : 
+// See tutorial at :
 // mouse hover effect
 // ===========================================================
 
@@ -200,7 +192,7 @@ new Swiper('.swiper-container', {
     },
     show: function () {
       return "(" + this.x + ", " + this.y + ")";
-    }
+    },
   };
 
   // Track the mouse position relative to the center of the container.
@@ -256,74 +248,69 @@ new Swiper('.swiper-container', {
   container.onmouseenter = onMouseEnterHandler;
 })();
 
-
-
-
-
 // ===========================================================
-// See tutorial at : 
+// See tutorial at :
 // mouse hover effect
 // cta
 // ===========================================================
-(function() {
+(function () {
   // Add event listener
   document.addEventListener("mousemove", parallax);
   const elem = document.querySelector("#cta-ani");
   // Magic happens here
   function parallax(e) {
-      let _w = window.innerWidth/2;
-      let _h = window.innerHeight/2;
-      let _mouseX = e.clientX;
-      let _mouseY = e.clientY;
-      let _depth1 = `${50 - (_mouseX - _w) * 0.10}% ${50 - (_mouseY - _h) * 0.10}%`;
-      let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
-      let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
-      let x = `${_depth3}, ${_depth2}, ${_depth1}`;
-      elem.style.backgroundPosition = x;
+    let _w = window.innerWidth / 2;
+    let _h = window.innerHeight / 2;
+    let _mouseX = e.clientX;
+    let _mouseY = e.clientY;
+    let _depth1 = `${50 - (_mouseX - _w) * 0.1}% ${50 - (_mouseY - _h) * 0.1}%`;
+    let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${
+      50 - (_mouseY - _h) * 0.02
+    }%`;
+    let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${
+      50 - (_mouseY - _h) * 0.06
+    }%`;
+    let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+    elem.style.backgroundPosition = x;
   }
-
 })();
 
-
-
-
 // ===========================================================
-// See tutorial at : 
+// See tutorial at :
 // mouse hover effect
 // ===========================================================
-(function() {
+(function () {
   // Add event listener
   document.addEventListener("mousemove", parallax);
   const elem = document.querySelector("#cta-footer");
   // Magic happens here
   function parallax(e) {
-      let _w = window.innerWidth/2;
-      let _h = window.innerHeight/2;
-      let _mouseX = e.clientX;
-      let _mouseY = e.clientY;
-      let _depth1 = `${50 - (_mouseX - _w) * 0.10}% ${50 - (_mouseY - _h) * 0.10}%`;
-      let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
-      let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
-      let x = `${_depth3}, ${_depth2}, ${_depth1}`;
-      elem.style.backgroundPosition = x;
+    let _w = window.innerWidth / 2;
+    let _h = window.innerHeight / 2;
+    let _mouseX = e.clientX;
+    let _mouseY = e.clientY;
+    let _depth1 = `${50 - (_mouseX - _w) * 0.1}% ${50 - (_mouseY - _h) * 0.1}%`;
+    let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${
+      50 - (_mouseY - _h) * 0.02
+    }%`;
+    let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${
+      50 - (_mouseY - _h) * 0.06
+    }%`;
+    let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+    elem.style.backgroundPosition = x;
   }
-
 })();
-
-
-
 
 //*****************/ The typewriter element*****************//
 
 // Type demo-6
 
-              
 var TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
   this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
+  this.txt = "";
   this.tick();
   this.isDeleting = false;
 };
@@ -332,60 +319,43 @@ TxtType.prototype.tick = function () {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
   if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
-  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
   var that = this;
   var delta = 200 - Math.random() * 100;
-  if (this.isDeleting) { delta /= 2; }
+  if (this.isDeleting) {
+    delta /= 2;
+  }
   if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
   }
   setTimeout(function () {
-      that.tick();
+    that.tick();
   }, delta);
 };
 
 function typewrite() {
-
-  if (toRotate === 'undefined') {
-      changeText()
-  }
-  else
-      var elements = document.getElementsByClassName('typewrite');
+  if (toRotate === "undefined") {
+    changeText();
+  } else var elements = document.getElementsByClassName("typewrite");
   for (var i = 0; i < elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-type');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-      }
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
   }
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
   document.body.appendChild(css);
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
