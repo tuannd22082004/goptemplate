ea7ec56a9b5fc94fe4e317b1ffd8df7c74387090
Nguyễn Đình Tuân <144453874+tuanndph33203@users.noreply.github.com>
2024-07-30 09:31:20 +0700

    feat: chuyển đổi tệp SCSS sang Bootstrap và 1 phần SCSS tại index-4  #WL-8
    - Thêm tệp SCSS mới: home_page_4.scss
    - Sửa đổi index-4.html với Bootstrap và SCSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,3 +5,4 @@ mix.sass("public/scss/variable.scss", "public/dist/css");
 
 mix.sass("public/scss/home_page.scss", "public/dist/css");
 mix.sass("public/scss/home_page_3.scss", "public/dist/css");
+mix.sass("public/scss/home_page_4.scss", "public/dist/css");
