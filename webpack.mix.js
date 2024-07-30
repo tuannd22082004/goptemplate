9651df80dcde9b405e3644965756728ceb6c70e2
Nguyễn Đình Tuân <tuannd@vmms.com.vn>
2024-07-30 11:45:07 +0700

    feat: Cập nhật index-5 với Bootstrap và 1 phần SCSS
    - Tạo file SCSS mới cho index-5: `public/scss/home_page_5.scss`
    - Cập nhật cấu trúc HTML cho trang chính 5 trong file: `public/index-5.html`
    - Cập nhật cấu hình Webpack trong file: `webpack.mix.js`

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,3 +6,4 @@ mix.sass("public/scss/variable.scss", "public/dist/css");
 mix.sass("public/scss/home_page.scss", "public/dist/css");
 mix.sass("public/scss/home_page_3.scss", "public/dist/css");
 mix.sass("public/scss/home_page_4.scss", "public/dist/css");
+mix.sass("public/scss/home_page_5.scss", "public/dist/css");
