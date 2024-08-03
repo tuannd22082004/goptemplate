586b056b31f6bd661706e75c5d3776c761b59052
Nguyễn Đình Tuân <tuannd@vmms.com.vn>
2024-08-03 10:38:27 +0700

    fix : Xóa app.js, mix-manifest.json thừa tại dist/js/

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,5 @@
 let mix = require("laravel-mix");
 
-mix.js('public/js/app.js', 'dist/js').setPublicPath('public');
-
 mix.sass("public/scss/global.scss", "public/dist/css");
 mix.sass("public/scss/common.scss", "public/dist/css");
 
