2f7979f634f0f2bc82eec44fd8bd1193c9ec18e3
Nguyễn Đình Tuân <tuannd@vmms.com.vn>
2024-08-06 14:50:19 +0700

    feat : Thay đổi Import Swiper Bằng CDN, thêm sử lý khởi tạo Swiper tại home-1.js

--- public/js/home-1.js
+++ public/js/home-1.js
@@ -39,85 +39,93 @@ function initMouseHoverEffect(containerId, innerId) {
     mouse.updatePosition(event);
     updateTransformStyle(
       (mouse.y / inner.offsetHeight / 5).toFixed(2),
-      (mouse.x / inner.offsetWidth / 2).toFixed(2)
+      (mouse.x / inner.offsetWidth / 2).toFixed(2),
     );
   };
 
   container.onmousemove = (event) => isTimeToUpdate() && update(event);
-  container.onmouseleave = () => (inner.style = "");
+  container.onmouseleave = () => (inner.style = '');
   container.onmouseenter = (event) => update(event);
 }
-initMouseHoverEffect("features-primary", "inner-primary");
-initMouseHoverEffect("features-secondary", "inner-secondary");
-function initSwipers() {
-  new Swiper(".brands__swiper", {
-    loop: true,
-    slidesPerView: "auto",
-    paginationClickable: true,
-    spaceBetween: 20,
-    centeredSlides: true,
-    speed: 1000,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      1920: { slidesPerView: 7, spaceBetween: 50 },
-      1028: { slidesPerView: 5, spaceBetween: 30 },
-      768: { slidesPerView: 3, spaceBetween: 10 },
-    },
-  });
+initMouseHoverEffect('features-primary', 'inner-primary');
+initMouseHoverEffect('features-secondary', 'inner-secondary');
 
-  new Swiper(".testimonial__swiper", {
-    loop: true,
-    slidesPerView: 3,
-    paginationClickable: true,
-    spaceBetween: 20,
-    pagination: ".swiper-pagination",
-    slidesPerView: "auto",
-    paginationClickable: true,
-    spaceBetween: 0,
-    centeredSlides: true,
-    speed: 1500,
-    breakpoints: {
-      1920: { slidesPerView: 3, spaceBetween: 50 },
-      1028: { slidesPerView: 2, spaceBetween: 30 },
-      768: { slidesPerView: 1, spaceBetween: 10 },
-    },
-  });
-}
-initSwipers();
+document.addEventListener('DOMContentLoaded', function () {
+  function initSwipers() {
+    new Swiper('.brands__swiper', {
+      loop: true,
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+      centeredSlides: true,
+      speed: 1000,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        1920: { slidesPerView: 9, spaceBetween: 50 },
+        1028: { slidesPerView: 6, spaceBetween: 25 },
+        768: { slidesPerView: 4, spaceBetween: 12 },
+        576: { slidesPerView: 3, spaceBetween: 12 },
+      },
+    });
 
+    new Swiper('.testimonial__swiper', {
+      loop: true,
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+      centeredSlides: true,
+      speed: 1500,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        1920: { slidesPerView: 3, spaceBetween: 50 },
+        1028: { slidesPerView: 3, spaceBetween: 20 },
+        768: { slidesPerView: 2, spaceBetween: 20 },
+        320: { slidesPerView: 1, spaceBetween: 10 },
+      },
+    });
+  }
+
+  initSwipers();
+});
 
 function applyParallax(selectors) {
-  selectors.forEach(selector => {
+  selectors.forEach((selector) => {
     const elem = document.querySelector(selector);
     if (!elem) {
       console.error(`Phần tử ${selector} không tồn tại trong DOM.`);
       return;
     }
 
-    document.addEventListener("mousemove", function parallax(e) {
+    document.addEventListener('mousemove', function parallax(e) {
       let _w = window.innerWidth / 2;
       let _h = window.innerHeight / 2;
       let _mouseX = e.clientX;
       let _mouseY = e.clientY;
-      let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
-      let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
-      let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+      let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${
+        50 - (_mouseY - _h) * 0.01
+      }%`;
+      let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${
+        50 - (_mouseY - _h) * 0.02
+      }%`;
+      let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${
+        50 - (_mouseY - _h) * 0.06
+      }%`;
       let x = `${_depth3}, ${_depth2}, ${_depth1}`;
       elem.style.backgroundPosition = x;
     });
   });
 }
-applyParallax([".banner__background", ".banner__round", ".cta", ".subscribe"]);
+applyParallax(['.banner__background', '.banner__round', '.cta', '.subscribe']);
 
-
-const counter = document.querySelectorAll(".counter__title");
+const counter = document.querySelectorAll('.counter__title');
 const speed = 35;
 counter.forEach((counter__title) => {
   const updateCount = () => {
-    const target = +counter__title.getAttribute("data-bs-target");
+    const target = +counter__title.getAttribute('data-bs-target');
     const count = +counter__title.innerText;
     const inc = target / speed;
     if (count < target) {
