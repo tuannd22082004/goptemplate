3ead24bb68563f95f26b06226459e7f58b55ea4c
Nguyễn Đình Tuân <tuannd@vmms.com.vn>
2024-08-06 10:57:38 +0700

    Feat : Cập nhật index.html , xoá các file không dùng, imporrt bootstrap,swiper bằng CDN #WL-8

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
     navbar.classList.remove("bg-color-7");
   });
 });
-
 function windowScroll() {
   const navbar = document.getElementById("navbar");
   if (
@@ -39,48 +38,3 @@ var TxtType = function (el, toRotate, period) {
   this.tick();
   this.isDeleting = false;
 };
-
-TxtType.prototype.tick = function () {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
-  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
-  var that = this;
-  var delta = 200 - Math.random() * 100;
-  if (this.isDeleting) {
-    delta /= 2;
-  }
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === "") {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-  setTimeout(function () {
-    that.tick();
-  }, delta);
-};
-
-function typewrite() {
-  if (toRotate === "undefined") {
-    changeText();
-  } else var elements = document.getElementsByClassName("typewrite");
-  for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute("data-type");
-    var period = elements[i].getAttribute("data-period");
-    if (toRotate) {
-      new TxtType(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-  document.body.appendChild(css);
-}
--- public/js/home-1.js
+++ public/js/home-1.js
@@ -58,7 +58,7 @@ function initSwipers() {
     centeredSlides: true,
     speed: 1000,
     autoplay: {
-      delay: 1,
+      delay: 3000,
       disableOnInteraction: false,
     },
     breakpoints: {
@@ -114,17 +114,14 @@ applyParallax([".banner__background", ".banner__round", ".cta", ".subscribe"]);
 
 
 const counter = document.querySelectorAll(".counter__title");
-const speed = 35; // The lower the slower
+const speed = 35;
 counter.forEach((counter__title) => {
   const updateCount = () => {
     const target = +counter__title.getAttribute("data-bs-target");
     const count = +counter__title.innerText;
     const inc = target / speed;
-    // Check if target is reached
     if (count < target) {
-      // Add inc to count and output in counter__title
       counter__title.innerText = (count + inc).toFixed(0);
-      // Call function every ms
       setTimeout(updateCount, 5);
     } else {
       counter__title.innerText = target;
