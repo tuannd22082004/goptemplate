ed26fe8b8ecc619a0f15799aaedfbc2f1ab62b43
Nguyễn Đình Tuân <tuannd@vmms.com.vn>
2024-08-13 11:16:35 +0700

    Feat : Complate Home Page 4 #WL-8

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,10 @@
-let mix = require("laravel-mix");
+let mix = require('laravel-mix');
 
-mix.js("public/js/app.js", "public/dist/js");
+mix.js('public/js/app.js', 'public/dist/js');
 
-mix.sass("public/scss/global.scss", "public/dist/css");
-mix.sass("public/scss/common.scss", "public/dist/css");
+mix.sass('public/scss/global.scss', 'public/dist/css');
+mix.sass('public/scss/common.scss', 'public/dist/css');
 
-mix.sass("public/scss/home_page.scss", "public/dist/css");
-mix.sass("public/scss/home_page_3.scss", "public/dist/css");
-mix.sass("public/scss/home_page_4.scss", "public/dist/css");
+mix.sass('public/scss/home_page.scss', 'public/dist/css');
+mix.sass('public/scss/home_page_3.scss', 'public/dist/css');
+mix.sass('public/scss/home_page_4.scss', 'public/dist/css');
